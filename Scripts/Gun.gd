727c91efaec3aa32013bd1ba236877b561552e9c
LeoGR22 <leonardogr2212@gmail.com>
2024-06-04 20:28:52 -0300

    Add Gun Anims

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -1,3 +1,18 @@
 extends Node3D
 
+@onready var anim = $AnimationPlayer
+var is_shooting : bool = false
 
+func _physics_process(delta):
+	if Input.is_action_just_pressed("Fire"):
+		anim.play("fire")
+		shoot()
+		is_shooting = true
+		await anim.animation_finished
+		is_shooting = false
+	else:
+		if not is_shooting:
+			anim.play("idle")
+
+func shoot():
+	pass
