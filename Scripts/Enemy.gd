2c32b088fa2a4c62f14b9d29c8d21f3dbd7d6de1
LeoGR22 <leonardogr2212@gmail.com>
2024-06-05 10:16:25 -0300

    Import new enemy

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -7,6 +7,8 @@ var canMove = false
 
 @onready var player = get_tree().get_first_node_in_group("Player")
 @onready var agent = $NavigationAgent3D
+@onready var anim = $AnimationPlayer
+
 
 var nextLocal
 var currentPos
@@ -21,7 +23,11 @@ func _physics_process(delta):
 		agent.set_target_position(player.global_transform.origin)
 		var next_nav_point = agent.get_next_path_position()
 		velocity = (next_nav_point - global_transform.origin).normalized() * speed
+		anim.play("Run")
 		
 		#Rotate
 		look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 		move_and_slide()
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
