478884aa23dd97af29d30f78dd8aba2e78b68c78
LeoGR22 <leonardogr2212@gmail.com>
2024-06-06 20:29:36 -0300

    Random spawnpoints for enemies

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -1,12 +1,15 @@
 class_name Spawner
 extends Node3D
 
+@onready var spawn_points = [$Spawn1, $Spawn2, $Spawn3, $Spawn4]
+
+
 var enemy = preload("res://Scenes/enemy.tscn")
 var spawned : bool = false
 
 func _ready():
+	randomize()
 	_spawn_enemy()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,6 +17,8 @@ func _process(delta):
 	
 func _spawn_enemy():
 	var new_enemy = enemy.instantiate()
+	var random_spawn_point = spawn_points[randi() % spawn_points.size()]
+	new_enemy.global_transform.origin = random_spawn_point.global_transform.origin
 	add_child(new_enemy)
 	await get_tree().create_timer(Manager.timeOfSpawnEnemies).timeout
 	_spawn_enemy()
